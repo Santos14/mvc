991300fcd46f1774cb1af7e6638209dd51c5b9bc
Sergio <sergio.12.educando@gmail.com>
2015-10-12 10:20:29 -0500

    Tercer Comiit

--- db/db.php
+++ db/db.php
@@ -1,8 +1,9 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class Conectar{
+    public static function conexion(){
+        $conexion = new mysqli("localhost","root","","ejemplo");
+        $conexion->query("SET NAMES 'utf8'");
+        return $conexion;
+    }
+}
